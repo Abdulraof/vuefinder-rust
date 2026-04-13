f4d5f73dcff3a2e66ddd72f8d1009a392a204021
Abdulraaof <abdulraofsh0@gmail.com>
2026-04-13 20:10:26 +0800

    feat: update payload structs to standardize path handling and add CopyRequest

--- src/payload.rs
+++ src/payload.rs
@@ -1,11 +1,17 @@
+use actix_multipart::form::{tempfile::TempFile, text::Text, MultipartForm};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Query {
-    pub q: String,
-    pub adapter: Option<String>,
-    pub path: Option<String>,
+    pub path: String,
+}
+
+#[derive(Deserialize)]
+pub struct SearchQuery {
+    pub path: String,
     pub filter: Option<String>,
+    pub deep: Option<bool>,
+    pub size: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -17,22 +23,33 @@ pub struct NewFolderRequest {
 #[derive(Deserialize)]
 pub struct NewFileRequest {
     pub name: String,
+    pub path: String,
 }
 
 #[derive(Deserialize)]
 pub struct RenameRequest {
-    pub name: String,
+    pub path: String,
     pub item: String,
+    pub name: String,
 }
 
 #[derive(Deserialize)]
 pub struct MoveRequest {
-    pub item: String,
-    pub items: Vec<FileItem>,
+    pub path: String,
+    pub sources: Vec<String>,
+    pub destination: String,
+}
+
+#[derive(Deserialize)]
+pub struct CopyRequest {
+    pub path: String,
+    pub sources: Vec<String>,
+    pub destination: String,
 }
 
 #[derive(Deserialize)]
 pub struct DeleteRequest {
+    pub path: String,
     pub items: Vec<FileItem>,
 }
 
@@ -40,19 +57,35 @@ pub struct DeleteRequest {
 pub struct ArchiveRequest {
     pub name: String,
     pub items: Vec<FileItem>,
+    pub path: String,
 }
 
 #[derive(Deserialize)]
 pub struct UnarchiveRequest {
     pub item: String,
+    pub path: String,
 }
 
 #[derive(Deserialize)]
 pub struct SaveRequest {
+    pub path: String,
     pub content: String,
 }
 
 #[derive(Deserialize)]
 pub struct FileItem {
     pub path: String,
+    pub r#type: String,
+}
+
+#[derive(Debug, MultipartForm)]
+pub struct UploadForm {
+    #[multipart(rename = "path")]
+    pub path: Text<String>,
+
+    #[multipart(rename = "name")]
+    pub name: Text<String>,
+
+    #[multipart(rename = "file")]
+    pub file: TempFile,
 }
