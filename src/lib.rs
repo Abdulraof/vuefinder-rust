832474e8af4f9a463a9f1b8c437873e8fb32b39e
Abdulraaof <abdulraofsh0@gmail.com>
2026-04-30 18:26:33 +0800

    Add thumbnail module to public exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod finder;
 pub mod payload;
 pub mod router;
 pub mod storages;
+pub mod thumbnail;
 
 pub use finder::{VueFinder, VueFinderConfig};
 pub use storages::{StorageAdapter, StorageItem};
 pub use storages::s3::{S3Config, S3Storage};
+pub use thumbnail::ThumbnailCache;
